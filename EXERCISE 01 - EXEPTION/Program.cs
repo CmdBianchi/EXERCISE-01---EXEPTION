f7cc4ef13fb2376efa9972f1e3880a7c9d5f56ae
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-12 17:48:00 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 01 - EXEPTION/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace EXERCISE_01___EXEPTION {
+    //-----> START
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    class Program {
+        static void Main(string[] args) {
+            try {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int resut = n1 / n2;
+                Console.WriteLine(resut);
+            }
+            catch (DivideByZeroException e){
+                Console.WriteLine("Error! " + e.Message);
+            }
+            catch (FormatException e) {
+                Console.WriteLine("Format error! " +e.Message);
+            }
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //-----> END
+}
